4bddd06a5208847b133e161210c3e95da74b60ec
willymasrur <binovdigital@gmail.com>
2025-12-05 05:10:55 +0700

    Update Banner.tsx

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Banner = () => {
 	return (
 		<>
-			<div>
+			<div className="container">
 				<div
 					className="card bg-primary mb-3 bg-img"
 					style={{ backgroundImage: `url(/assets/img/core-img/1.png)` }}
